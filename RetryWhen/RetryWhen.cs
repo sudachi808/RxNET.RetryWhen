443340b2f53755dfc719357cd0df2d18cd96c287
Atsushi Kimura <sudachi808@gmail.com>
2017-06-24 15:43:56 +0900

    first commit

---
+++ RetryWhen/RetryWhen.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace RxNET.Extensions
+{
+    public static class RetryWhenExtenstion
+    {
+        public static IObservable<T> RetryWhen<T>(
+            this IObservable<T> source,
+            Func<IObservable<Exception>, IObservable<T>> predicate)
+        {
+            return RetryWhenRecursive(source, predicate);
+        }
+        
+        private static IObservable<T> RetryWhenRecursive<T>(
+            IObservable<T> source,
+            Func<IObservable<Exception>, IObservable<T>> predicate)
+        {
+            return source.Catch((Exception e) =>
+            {
+                //
+                // シーケンスから例外が Throw された場合、
+                // predicate でリトライするかどうかを判定する。
+                //
+                return
+                     predicate(Observable.Return(e))
+                    .Catch((Exception ee) =>
+                     {
+                        //
+                        // predicate から例外が Throw された場合、
+                        // 後続のシーケンスに例外を流す（＝リトライせずオブザーバーに例外を捕捉させる）。
+                        //
+                        return Observable.Throw<T>(e);
+                     })
+                    .SelectMany(_ =>
+                     {
+                        //
+                        // predicate から例外が Throw されなかった場合、
+                        // 再度、起点となったシーケンスに繋げる（＝リトライ）。
+                        //
+                        return RetryWhenRecursive(source, predicate);
+                     });
+            });
+        }
+    }
+}
---
+++ RetryWhenDemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Reactive.Linq;
+
+using RxNET.Extensions;
+
+namespace RetryWhenDemo
+{
+    class MainClass
+    {
+        const int RETRY_MAX  = 10;
+        const int RETRY_WAIT = 1;
+
+        public static void Main(string[] args)
+        {
+            var service = new GreetingService();
+
+            int retryAttempt = 0;
+
+            service.Greet()
+                   .RetryWhen(errors => {
+                        return errors.SelectMany(error => {
+                            if (error is NeedToRetryException && ++retryAttempt <= RETRY_MAX) {
+                                return Observable.Return(String.Empty).Delay(TimeSpan.FromSeconds(RETRY_WAIT));
+                            }
+                            return Observable.Throw<string>(error);
+                        });
+                    })
+                   .Subscribe(
+                        greet => Console.WriteLine("OnNext: {0}", greet),
+                        error => Console.WriteLine("OnError: {0}", error.Message),
+                        ()    => Console.WriteLine("OnCompleted:")
+                  );
+
+            Console.ReadKey();
+        }
+    }
+    
+    internal class GreetingService
+    {
+        int attempt = 0;
+
+        public IObservable<string> Greet()
+        {
+            return Observable.Defer(() => {
+                Console.WriteLine("Defer [{0}]", ++attempt);
+                if (attempt % 5 == 0) {
+                    return Observable.Return("Hello");
+                }
+                return Observable.Throw<string>(new NeedToRetryException("Zzz..."));
+            });
+        }
+    }
+
+    class NeedToRetryException : Exception
+    {
+        public NeedToRetryException() : base() { }
+        public NeedToRetryException(string message) : base(message) { }
+        public NeedToRetryException(string message, Exception inner) : base(message, inner) { }
+    }
+}
